5165d7c305b519f1db0ac5806ee96604f09f380d
joshstenis <joshstenis@gmail.com>
2020-12-01 20:15:13 -0600

    Coded structure, including Node struct

--- cs2413_hw5.cpp
+++ cs2413_hw5.cpp
@@ -17,16 +17,116 @@ int strToInt(string str) {
     return val/10;
 }
 
+class Node {
+        int value;
+        Node* parent;
+        Node* rightChild;
+        Node* leftChild;
+
+    public:
+
+        Node(int val, Node* par, Node* right, Node* left) {
+            value = val;
+            rightChild = right;
+            leftChild = left;
+            parent = par;
+        }
+
+        Node(int val, Node* right, Node* left) {                 // Root constr
+            value = val;
+            rightChild = right;
+            leftChild = left;
+            parent = NULL;
+        }
+
+        Node(int val, Node* par) {                              // Leaf constr
+            value = val;
+            rightChild = NULL;
+            leftChild = NULL;
+            parent = par;
+        }
+
+        /**
+         * Outputs the result of a search
+         * @param k the key
+         */
+        void search(int k, Node* n) {
+            if(n == NULL) cout << -1;
+            else if(n->getValue() < k) search(k, n->getRightChild());
+            else if(n->getValue() > k) search(k, n->getLeftChild());
+            else cout << n;
+        }
+
+        /**
+         * Returns the value
+         * @return the value
+         */
+        int getValue() {
+            return value;
+        }
+
+        /**
+         * Sets value
+         * @param v new value
+         */
+        void setValue(int v) {
+            value = v;
+        }
+
+        /**
+         * Returns ptr to the right child (>)
+         * @return the right child
+         */
+        Node* getRightChild() {
+            return rightChild;
+        }
+
+        /**
+         * Sets right child
+         * @param r new right child
+         */
+        void setRightChild(Node* r) {
+            rightChild = r;
+        }
+
+        /**
+         * Returns ptr to the left child (<)
+         * @return the left child
+         */
+        Node* getLeftChild() {
+            return leftChild;
+        }
+
+        /**
+         * Sets left child
+         * @param l new left child
+         */
+        void setLeftChild(Node* l) {
+            leftChild = l;
+        }
+};
+
+/**
+ * Creates a binary search tree based on vector of keys
+ * @param keys the keys
+ * @return the head node
+ */
+Node* createTree(vector<int> keys) {
+    Node* head = new Node(keys[0], NULL, NULL);
+
+    return head;
+}
+
 int main() {
     string val;
     int op, key;
     vector<int> init;
 
-    while(!cin.fail()) {                        // Takes in keys for initial tree
+    while(!cin.fail()) {                        // Takes input and creates initial tree
         cin >> val;
         if(val == "s") break;
         init.push_back(strToInt(val));
-    }
+    } Node* head = createTree(init);
 
     cin.ignore(1, '\n');
     cin >> op;
@@ -34,9 +134,7 @@ int main() {
     cin.ignore(1, '\n');
     cin >> key;
 
-    cin.ignore(1, '\n');
-
-    switch(op) {
+    switch(op) {                                // Operation
         case 1:
         {
             // Search
